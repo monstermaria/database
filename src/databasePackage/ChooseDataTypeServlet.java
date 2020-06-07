b71ec9d7d5f97d9bc8ef76e1689a2eb4c4dfe7b5
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-06-07 20:49:28 +0200

    Changed route to match servlet name

--- src/databasePackage/ChooseDataTypeServlet.java
+++ src/databasePackage/ChooseDataTypeServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ChooseTableServlet
  */
-@WebServlet("/ChooseTableServlet")
+@WebServlet("/ChooseDataTypeServlet")
 public class ChooseDataTypeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
